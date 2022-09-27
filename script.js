30b93516b86b9c31a3b1bae6a03278e308611aff
tero27 <fmaderni@gmail.com>
2022-09-27 19:22:04 -0300

    calculator working, finally... Missing factorial function and color

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let operator = '';
 let currentValue = '';
 let previousValue = '';
+let aux;
 
 const btn = document.querySelectorAll('.button');
 const crntDspl = document.querySelector('.current');
@@ -13,29 +14,11 @@ const chgsgn = document.getElementById('inv');
 const coma = document.getElementById('coma');
 const res = document.getElementById('equ');
 
-
 clear.addEventListener('click', clearDisp);
-dlt.addEventListener('click', deleteNumb);
+dlt.addEventListener('click', deleteNum);
 chgsgn.addEventListener('click', invert);
 coma.addEventListener('click', insertComa);
-res.addEventListener('click', calculate);
 
-
-function add(a, b) {
-    return a + b;
-};
-
-function substract(a, b) {
-    return a - b;
-};
-
-function product(a, b) {
-    return a * b;
-};
-
-function divide(a, b) {
-    return a / b;
-};
 
 function fact(a) {
     var total = a
@@ -48,51 +31,82 @@ function fact(a) {
     return total;
 };
 
-
-function readOp(a, b, op) {
-    op == '+' ? addNum(a, b) :
-        op == '-' ? substract(a, b) :
-            op == 'x' ? product(a, b) :
-                op == '!' ? fact(a) :
-                    divide(a, b);
-}
-
 function clearDisp() {
-    crntDspl.innerHTML = '';
-}
+    currentValue = '';
+    previousValue = '';
+    crntDspl.innerText = '';
+    prvsDspl.innerText = '';
+};
 
-function deleteNumb() {
-    crntDspl.innerHTML = crntDspl.innerHTML.slice(0, -1)
-}
+function deleteNum() {
+    crntDspl.innerText = crntDspl.innerText.slice(0, -1);
+    currentValue = crntDspl.innerText;
+};
 
 function invert() {
-    const neg = -Number(crntDspl.innerHTML);
-    crntDspl.innerHTML = neg;
-    console.log(neg);
-    console.log(typeof(neg))
-}
+    const neg = -Number(crntDspl.innerText);
+    crntDspl.innerText = neg;
+};
 
 function insertComa() {
-    if (crntDspl.innerHTML === '')
-        crntDspl.innerHTML = '0';
-    if (!crntDspl.innerHTML.includes(','))
-        return crntDspl.innerHTML += ',';
-}
+    if (crntDspl.innerText === '')
+        crntDspl.innerText = '0';
+    if (!crntDspl.innerText.includes(','))
+        crntDspl.innerText += ',';
+    currentValue = crntDspl.innerText;
+};
 
-function display(e) {
-    const value = e.target.dataset.value;
-    crntDspl.innerHTML += value;
-}
+function display(a) {
+    if (currentValue.length < 20) {
+        currentValue += a;
+    }
+};
+
+function operate(op) {
+    if (!currentValue.includes(op)) {
+        currentValue += ' ' + op;
+        previousValue = currentValue;
+        currentValue = '';
+        operator = op;
+    }
+};
 
 function calculate() {
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+    aux = previousValue + " " + operator + " " + currentValue
 
-}
+    if (operator === '+') {
+        previousValue += currentValue;
+    } else if (operator === '-') {
+        previousValue -= currentValue;
+    } else if (operator === 'x') {
+        previousValue *= currentValue
+    } else {
+        previousValue /= currentValue;
+    };
+
+    currentValue = previousValue.toString()
+    previousValue = aux.toString();
+};
 
 btn.forEach(button =>
-    button.addEventListener('click', display)
-);
+    button.addEventListener('click', function (e) {
+        display(e.target.innerText)
+        crntDspl.innerText = currentValue;
+    }));
 
 oper.forEach(button =>
-    button.addEventListener('click', display)
+    button.addEventListener('click', function (e) {
+        operate(e.target.dataset.value);
+        previousValue = previousValue.slice(0, -2);
+        prvsDspl.innerText = previousValue + " " + operator;
+        crntDspl.innerText = currentValue;
+    })
 );
 
+res.addEventListener('click', function () {
+    calculate();
+    prvsDspl.innerText = previousValue
+    crntDspl.innerHTML = currentValue;
+});
