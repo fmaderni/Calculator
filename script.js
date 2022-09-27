ca29366dce90948afdbdc936ae4e1732bbf7417a
tero27 <fmaderni@gmail.com>
2022-09-26 22:32:26 -0300

    added functionality to AC and C buttons

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
 let btn = document.querySelectorAll('.button');
 let dspl = document.querySelector('.display');
-let oper = document.querySelectorAll('.op');
+let val1;
+let val2;
+let operator = ['+','-','x','÷','!']
 
+function add(a, b) {
+    return a + b;
+};
 
+function substract(a, b) {
+    return a - b;
+};
+
+function product(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+}
+
+function readOp(a, b, op) {
+    op == '+' ? addNum(a, b) :
+        op == '-' ? substract(a, b) :
+            op == 'x' ? product(a, b) :
+                divide(a, b);
+}
 
 
 btn.forEach((button) => {
     button.addEventListener('click', (e) => {
-        dspl.innerText = e.target.innerText;
-    })
+        const value = e.target.dataset.value;
+        dspl.innerHTML += value;
+        let innit = dspl.innerHTML
+        if(value == 'AC'){
+            dspl.innerHTML = '';
+        }
+        if(value == 'C'){
+            let val1 = innit.slice(0,innit.length - 2);
+            dspl.innerHTML = val1;
+        }
+    });
 });
+
